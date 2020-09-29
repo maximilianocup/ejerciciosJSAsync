3abadb8dbad4d0c6b09a840073a19f9f474d44c2
maximilianocup <63514082+maximilianocup@users.noreply.github.com>
2020-09-28 23:44:46 -0300

    Update app.js
    terminado

--- ejercicio_4/app.js
+++ ejercicio_4/app.js
@@ -1,5 +1,8 @@
-
 const obtenerPokemon = require("./library");
 
 // Usar la funcion obtenerChiste(nombrePokemon) la cual recibe como string un nombre de pokemon y devuelve la promesa de traer el objeto pokemon extraido
-obtenerPokemon();
+obtenerPokemon("charizard").then(data => {
+    console.log("Nombre: " + data.name);
+    console.log("Habilidades: ")
+    data.abilities.forEach(i => console.log(i.ability.name));
+});
